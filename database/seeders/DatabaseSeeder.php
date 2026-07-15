a414a9430a12e46f16f71ffe599166dbf9e42cbb
Ryan Enns <ryanennns@gmail.com>
2026-07-15 15:06:07 -0400

    remove admin seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $password = config('registration.admin_password');
-
-        if (! is_string($password) || $password === '') {
-            throw new \LogicException('Set ADMIN_PASSWORD before seeding the administrator.');
-        }
-
-        User::query()->updateOrCreate(['email' => 'ryanennns@gmail.com'], [
-            'name' => 'Ryan Enns',
-            'password' => Hash::make($password),
-        ]);
     }
 }
